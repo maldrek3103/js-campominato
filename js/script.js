b86e602cbbb50b0380b56008c4f0f7c21030d780
maldrek3103 <cpisanu96@gmail.com>
2021-07-13 15:58:49 +0200

    added ask to user loop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // creare un array per contenere le bombe
 const bombs = [];
-
+const easy = 100;
+const medium = 80;
+const hard = 50;
 //generare un numero random da inserire nell'array bombe finchè non ci saranno 16 elementi (funzione)
 function bombGenerator(min, max) {
     for (var i = 0; i < 16; i++) {
@@ -11,3 +13,20 @@ function bombGenerator(min, max) {
 
 console.log(bombGenerator(1, 100));
 console.table(bombs);
+
+// creare un array per ricordare i numeri scelti dall'utente
+const userNumbers = [];
+//chiede all'utente di scegliere un numero da 1 a 100
+let askUser = prompt('Scegli un numero da 1 a 100');
+//inserisce il numero scelto dall'utente dentro l'arrey userNumbers
+userNumbers.push(askUser);
+
+
+//chiede 100 -16 volte all'utente di scrivere un numero da 1 a 100 ( da finire)
+for (var i = 0; i < (easy - bombs.length); i++) {
+    askUser = prompt('Scegli un numero da 1 a 100');
+
+}
+
+
+console.log(userNumbers);
