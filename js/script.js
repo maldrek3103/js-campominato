f5f6535a113468c307c060e59e3a7f63593e7787
maldrek3103 <cpisanu96@gmail.com>
2021-07-15 15:57:41 +0200

    Improved script, made categories, changed variables

--- js/script.js
+++ js/script.js
@@ -1,52 +1,104 @@
-// creare un array per contenere le bombe
-const bombs = [];
-const easy = 100;
-const medium = 80;
-const hard = 50;
+//============ FIXED VARIABLES ==============
+//(REMEMBER TO REPLACE AND REMOVE TEST VALUES)
+const totalNumbers = 10;     //Replace with 100
+const totalBombs = 5;       //Replace with 16
+let maxAttempts = totalNumbers - totalBombs;
 
+//=========== CHANGING VARIABLES ============
+const bombs = [1, 3, 5, 7, 9];        //Remove elements
+const userNumbers = [];
 
-//generare un numero random da inserire nell'array bombe finchè non ci saranno 16 elementi (funzione)
-function bombGenerator(min, max) {
-    for (var i = 0; i < 16; i++) {
-        let randomBomb = Math.floor(Math.random() * (max - min) + min);
-        bombs.push(randomBomb);
+//============ CHECKING VARIABLES ============
+let isLost = false;
+
+//================= GAME ====================
+// Bombs generator with vefication already exixting bombs
+while (bombs.length < totalBombs) {
+
+    // Generates a random number between 1 and 100
+    let randomNumber = getRandomNumber(1, totalNumbers);
+
+    // Verify that the generated number is not in the array 'bombs' yet
+    if (!isInArray(randomNumber, bombs)) {
+        bombs.push(randomNumber);
     }
 }
 
-console.log(bombGenerator(1, 100));
-console.table(bombs);
+//Validates the user inputs
+while (!isLost && userNumbers.length < maxAttempts) {
 
+    //Ask user input as a number between min and max
+    let userChoice = getUserNumber(1, totalNumbers);
 
-// creare un array per ricordare i numeri scelti dall'utente
-const userNumbers = [];
-
-
-//chiede all'utente di scegliere un numero da 1 a 100
-let askUser = prompt('Scegli un numero da 1 a 100');
-
-
-//inserisce il numero scelto dall'utente dentro l'arrey userNumbers
-userNumbers.push(askUser);
-
-
-//chiede 100 -16 volte all'utente di scrivere un numero da 1 a 100 ( da finire)
-for (var i = 0; i < (easy - bombs.length); i++) {
-    askUser = prompt('Scegli un numero da 1 a 100');
-
+    //Check if the number is not in the 'bombs' array yet
+    if (isInArray(userChoice, bombs)) {
+        isLost = true;
+    } else {
+        //Checks if the number is not in the array 'userNumbers' yet
+        if (isInArray(userChoice, userNumbers)) {
+            alert('Il numero è gia stato scelto')
+        } else {
+            userNumbers.push(userChoice);
+        }
+    }
 }
-
-
-console.log(userNumbers);
-
-
-// controllo numero dell'utente con bombe, se il numero scelto dall'utente corrisponde ad un numero
-// nell'array delle bombe oppure ad un numero gia scelto, se è uguale ad una bomba termina gioco,
-// altrimenti aggiungi numero all'array del giocatore
-if (userNumbers === bombs) {
-    console.log('Hai perso');
-} else if (userNumbers === askUser) {
-    console.log('Mi dispiace hai gia scelto questo numero');
+if (isLost) {
+    alert('Hai perso! Hai totalizzato: ' + userNumbers.length);
 } else {
-    userNumbers.push(askUserNumber);
+    alert('Hai vinto!');
 }
 
+//========= FUNCTIONS DECLARATIONS ==========
+/**
+ * Generates a random number between min and max
+ * @param {number} min 
+ * @param {number} max 
+ */
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * Checks if a element is in an array
+ * @param {*} element 
+ * @param {*} arr 
+ */
+function isInArray(element, arr) {
+    let found = false;
+    let i = 0;
+    while (!found && i < arr.length) {
+        if (element === arr[i]) {
+            found = true;
+        }
+        i++;
+    }
+    return found;
+}
+
+/**
+ * Ask user a number until it is not a valid element (between min and max)
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
+function getUserNumber(min, max) {
+    //Ask user number
+    let number = prompt('Inserisci un numero da ' + min + ' a ' + max);
+    while (!isNumber(number) || number < 1 || number > max) {
+        number = prompt('Inserisci un numero da ' + min + ' a ' + max);
+    }
+    return parseInt(number);
+}
+
+/**
+ * Checks if a value is a number
+ * @param {*} num 
+ * @returns 
+ */
+function isNumber(num) {
+    if (!num || num.trim() === '' || isNaN(num)) {
+        alert('Devi inserire un valore valido!');
+        return false;
+    }
+    return true;
+}
